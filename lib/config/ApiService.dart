bf1eb0587d9e36c412810bf803f0034675eaf4ff
Ibrahim <ibrahimjkxd@Gmail.com>
2026-05-21 02:46:46 +0500

    created config file

---
+++ lib/config/ApiService.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Apiservice {
+
+  static const String baseUrl = "http://10.0.2.2:5000";
+
+  static const String employees = "$baseUrl/employees";
+  static const String department = "$baseUrl/departments";
+
+
+}
